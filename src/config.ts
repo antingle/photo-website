179647a378b38c93367c16d20de961a10b316bac
Anthony Ingle <ingle.anthony@icloud.com>
2023-03-19 01:50:20 -0400

    added contact page

--- src/config.ts
+++ src/config.ts
@@ -28,8 +28,35 @@ const SOCIAL: SocialLink[] = [
   },
 ];
 
+const CONTACT: SocialLink[] = [
+  {
+    icon: "mdi:facebook",
+    ariaLabel: "Facebook Page",
+    href: "https://www.facebook.com/anthonyinglephotography",
+    customClasses: ["hover:text-blue-600"],
+  },
+  {
+    icon: "mdi:instagram",
+    ariaLabel: "Instagram Page",
+    href: "https://www.instagram.com/ant.ing/",
+    customClasses: ["hover:text-fuchsia-600"],
+  },
+  {
+    icon: "ic:baseline-phone",
+    ariaLabel: "Phone Number",
+    href: "tel:7273310866",
+    customClasses: ["hover:text-gray-600"],
+  },
+  {
+    icon: "ic:round-mail-outline",
+    ariaLabel: "Email",
+    href: "mailto:anthony@anthonyinglephotos.com",
+    customClasses: ["hover:text-teal-800"],
+  },
+];
+
 const NAME = "Anthony Ingle Photography";
 const DESCRIPTION =
   "Anthony Ingle is a photographer located in Tallahassee, Florida and the Florida State University area";
 
-export { LINKS, NAME, DESCRIPTION, SOCIAL, ACTIONS };
+export { LINKS, NAME, DESCRIPTION, SOCIAL, ACTIONS, CONTACT };
